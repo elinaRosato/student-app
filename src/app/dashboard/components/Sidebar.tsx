4cbc9e3fb83b3a6b4b81e7676ca0f43a019d5b1c
Elina <rosatoelina@gmail.com>
2024-11-24 18:26:29 +0100

    style(sidebar): update styles to highlight active link

--- src/app/dashboard/components/Sidebar.tsx
+++ src/app/dashboard/components/Sidebar.tsx
@@ -4,10 +4,12 @@ import Link from 'next/link';
 import { logout } from '@/app/(pages)/login/actions';
 import { DashboardCircleIcon, CourseIcon, Notebook01Icon, Task01Icon, Files02Icon, Settings02Icon, CustomerService01Icon, Logout03Icon, CrownIcon, HelpCircleIcon, ArrowLeft01Icon, ArrowRight01Icon, NoteIcon } from "hugeicons-react";
 import { useState } from 'react';
+import { usePathname } from 'next/navigation';
 
 
 
 const Sidebar = () => {
+  const pathname = usePathname();
   const [showSidebar, setShowSidebar] = useState(true);
 
   function toggleShowHideSidebar () {
@@ -15,13 +17,13 @@ const Sidebar = () => {
   }
 
   return (
-    <div className=" bg-gray-200 text-gray-500 flex flex-col m-2 rounded-lg p-4">
-      <div className={`flex gap-2 items-center h-20 text-gray-700 text-xl font-semibold ${showSidebar ? 'justify-start' : 'justify-center'}`}>
+    <div className=" bg-slate-200 text-slate-500 flex flex-col m-2 rounded-lg p-4">
+      <div className={`flex gap-2 items-center h-20 text-slate-700 text-xl font-semibold ${showSidebar ? 'justify-start' : 'justify-center'}`}>
         <NoteIcon size={24}/>
         <span>{showSidebar ? 'StudentApp' : ''}</span>
       </div>
       <div className={`flex ${showSidebar ? 'justify-end' : 'justify-center'}`}>
-        <button className='bg-gray-300 hover:bg-blue-200 rounded-lg' onClick={toggleShowHideSidebar}>
+        <button className='bg-slate-300 hover:bg-blue-200 rounded-lg' onClick={toggleShowHideSidebar}>
           {showSidebar ? <ArrowLeft01Icon size={24}/> : <ArrowRight01Icon size={24}/>}
         </button>
         
@@ -29,12 +31,12 @@ const Sidebar = () => {
       <div className="flex-grow overflow-y-auto flex flex-col justify-between">
         <div>
           <div className='py-4'>
-            <h3 className="text-gray-500 text-m font-medium uppercase py-2" >{showSidebar ? 'General' : ''}</h3>
-            <ul className="space-y-2 text-gray-700 ">
+            <h3 className="text-slate-500 text-m font-medium uppercase py-2" >{showSidebar ? 'General' : ''}</h3>
+            <ul className="space-y-2 text-slate-700 ">
               <li>
                 <Link
                   href="/dashboard"
-                  className={`block w-full text-left p-3 rounded-lg flex gap-2 hover:bg-gray-300 focus:outline-none ${showSidebar ? 'justify-start' : 'justify-center'}`}
+                  className={`block w-full text-left p-3 rounded-lg flex gap-2 focus:outline-none ${pathname === '/dashboard' ? 'bg-blue-300' : 'hover:bg-slate-300'} ${showSidebar ? 'justify-start' : 'justify-center'}`}
                 >
                   <DashboardCircleIcon size={24}/>
                   {showSidebar ? 'Dashboard' : ''}
@@ -43,7 +45,7 @@ const Sidebar = () => {
               <li>
                 <Link
                   href="/dashboard/courses"
-                  className={`block w-full text-left p-3 rounded-lg flex gap-2 hover:bg-gray-300 focus:outline-none ${showSidebar ? 'justify-start' : 'justify-center'}`}
+                  className={`block w-full text-left p-3 rounded-lg flex gap-2 focus:outline-none ${pathname === '/dashboard/courses' ? 'bg-blue-300' : 'hover:bg-slate-300'} ${showSidebar ? 'justify-start' : 'justify-center'}`}
                 >
                   <CourseIcon size={24}/>
                   {showSidebar ? 'Courses' : ''}
@@ -52,7 +54,7 @@ const Sidebar = () => {
               <li>
                 <Link
                   href="/dashboard/notebooks"
-                  className={`block w-full text-left p-3 rounded-lg flex gap-2 hover:bg-gray-300 focus:outline-none ${showSidebar ? 'justify-start' : 'justify-center'}`}
+                  className={`block w-full text-left p-3 rounded-lg flex gap-2 focus:outline-none ${pathname === '/dashboard/notebooks' ? 'bg-blue-300' : 'hover:bg-slate-300'} ${showSidebar ? 'justify-start' : 'justify-center'}`}
                 >
                   <Notebook01Icon size={24}/>
                   {showSidebar ? 'Notebooks' : ''}
@@ -61,7 +63,7 @@ const Sidebar = () => {
               <li>
                 <Link
                   href="/dashboard/tasks"
-                  className={`block w-full text-left p-3 rounded-lg flex gap-2 hover:bg-gray-300 focus:outline-none ${showSidebar ? 'justify-start' : 'justify-center'}`}
+                  className={`block w-full text-left p-3 rounded-lg flex gap-2 focus:outline-none ${pathname === '/dashboard/tasks' ? 'bg-blue-300' : 'hover:bg-slate-300'} ${showSidebar ? 'justify-start' : 'justify-center'}`}
                 >
                   <Task01Icon size={24}/>
                   {showSidebar ? 'Tasks' : ''}
@@ -70,7 +72,7 @@ const Sidebar = () => {
               <li>
                 <Link
                   href="/dashboard/resources"
-                  className={`block w-full text-left p-3 rounded-lg flex gap-2 hover:bg-gray-300 focus:outline-none ${showSidebar ? 'justify-start' : 'justify-center'}`}
+                  className={`block w-full text-left p-3 rounded-lg flex gap-2 focus:outline-none ${pathname === '/dashboard/resources' ? 'bg-blue-300' : 'hover:bg-slate-300'} ${showSidebar ? 'justify-start' : 'justify-center'}`}
                 >
                   <Files02Icon size={24}/>
                   {showSidebar ? 'Resources' : ''}
@@ -80,12 +82,12 @@ const Sidebar = () => {
           </div>
           
           <div className='py-4'>
-            <h3 className="text-gray-500 text-m font-medium uppercase py-2" >{showSidebar ? 'More' : ''}</h3>
-            <ul className="space-y-2 text-gray-700 ">
+            <h3 className="text-slate-500 text-m font-medium uppercase py-2" >{showSidebar ? 'More' : ''}</h3>
+            <ul className="space-y-2 text-slate-700 ">
               <li>
                 <Link
                   href="/dashboard/settings"
-                  className={`block w-full text-left p-3 rounded-lg flex gap-2 hover:bg-gray-300 focus:outline-none ${showSidebar ? 'justify-start' : 'justify-center'}`}
+                  className={`block w-full text-left p-3 rounded-lg flex gap-2 focus:outline-none ${pathname === '/dashboard/settings' ? 'bg-blue-300' : 'hover:bg-slate-300'} ${showSidebar ? 'justify-start' : 'justify-center'}`}
                 >
                   <Settings02Icon size={24}/>
                   {showSidebar ? 'Settings' : ''}
@@ -94,7 +96,7 @@ const Sidebar = () => {
               <li>
                 <Link
                   href="/dashboard/support"
-                  className={`block w-full text-left p-3 rounded-lg flex gap-2 hover:bg-gray-300 focus:outline-none ${showSidebar ? 'justify-start' : 'justify-center'}`}
+                  className={`block w-full text-left p-3 rounded-lg flex gap-2 focus:outline-none ${pathname === '/dashboard/support' ? 'bg-blue-300' : 'hover:bg-slate-300'} ${showSidebar ? 'justify-start' : 'justify-center'}`}
                 >
                   <CustomerService01Icon size={24}/>
                   {showSidebar ? 'Support' : ''}
@@ -103,7 +105,7 @@ const Sidebar = () => {
               <li>
                 <Link
                   href="/dashboard/support"
-                  className={`block w-full text-left p-3 rounded-lg flex gap-2 hover:bg-gray-300 focus:outline-none ${showSidebar ? 'justify-start' : 'justify-center'}`}
+                  className={`block w-full text-left p-3 rounded-lg flex gap-2 focus:outline-none ${pathname === '/dashboard/help' ? 'bg-blue-300' : 'hover:bg-slate-300'} ${showSidebar ? 'justify-start' : 'justify-center'}`}
                 >
                   <HelpCircleIcon size={24}/>
                   {showSidebar ? 'Help' : ''}
@@ -115,23 +117,23 @@ const Sidebar = () => {
         
         <div className='self-end'>
           
-          <button className={`block w-full text-left p-3 my-4 text-gray-700 rounded-lg flex gap-2 bg-blue-300 hover:bg-blue-400 focus:outline-none ${showSidebar ? 'justify-start' : 'justify-center'}`}>
+          <button className={`block w-full text-left p-3 my-4 text-slate-200 rounded-lg flex gap-2 bg-blue-400 hover:bg-blue-500 focus:outline-none ${showSidebar ? 'justify-start' : 'justify-center'}`}>
             <CrownIcon size={24}/>
             {showSidebar ? 'Upgrade to pro' : ''}
           </button>
 
-          <button className={`block w-full text-left p-3 text-gray-700 rounded-lg flex gap-2 hover:bg-gray-300 focus:outline-none${showSidebar ? 'justify-start' : 'justify-center'}`} onClick={() => logout()}>
+          <button className={`block w-full text-left p-3 text-slate-500 rounded-lg flex gap-2 hover:bg-slate-300 focus:outline-none ${showSidebar ? 'justify-start' : 'justify-center'}`} onClick={() => logout()}>
             <Logout03Icon size={24}/>
             {showSidebar ? 'Logout' : ''}
           </button>
 
           <div className={`flex gap-2 py-4 ${showSidebar ? 'justify-start' : 'justify-center'}`}>
-            <div className='w-12 h-12 bg-blue-400 rounded-full flex' >
-              <div className='w-4 h-4 bg-green-500 rounded-full border-2 border-gray-200'></div>
+            <div className='w-12 h-12 bg-blue-400 rounded-full flex justify-end' >
+              <div className='w-4 h-4 bg-green-500 rounded-full border-2 border-slate-200'></div>
             </div>
             <div>
-              <p className='text-gray-500 text-s font-medium'>{showSidebar ? 'Elina Rosato' : ''}</p>
-              <p className='text-gray-400 text-s'>{showSidebar ? 'rosatoelina@gmail.com' : ''}</p>
+              <p className='text-slate-500 text-s font-medium'>{showSidebar ? 'Elina Rosato' : ''}</p>
+              <p className='text-slate-400 text-s'>{showSidebar ? 'rosatoelina@gmail.com' : ''}</p>
             </div>
           </div>
         </div>
