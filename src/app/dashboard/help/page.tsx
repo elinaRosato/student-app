64b8a8c3c77014a7f0536c4fd30f24713abefc0f
Elina <rosatoelina@gmail.com>
2024-11-24 18:31:58 +0100

    feat(sidebar): create help directory with page.tsx for new sidebar element

---
+++ src/app/dashboard/help/page.tsx
@@ -0,0 +1,10 @@
+const Help = () => {
+    return (
+      <>
+        <h1 className="text-3xl font-bold">Help</h1>
+        {/* Your dashboard content here */}
+      </>
+    );
+  };
+  
+  export default Help;
